f125d70dc0dd460468732739bb2837462cafea51
SumanMore <67056908+SumanMore@users.noreply.github.com>
2020-06-17 20:31:41 +0530

    Update q9.java

--- q9.java
+++ q9.java
@@ -5,9 +5,9 @@ package code;
 public class q9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int a=0,b=0,c=0,s=1000;
-	    int ctr=0;
+	    int d=0;
 	    for(a=1;a<s/3;a++)
 	    {
 	        for(b=a;b<s/2;b++)
