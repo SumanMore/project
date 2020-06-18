0edc91d2627a85df4c0cf55f86bb6033824572ce
SumanMore <67056908+SumanMore@users.noreply.github.com>
2020-06-18 10:34:25 +0530

    Update q7.java

--- q7.java
+++ q7.java
@@ -1,29 +1,21 @@
-//By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 //What is the 10 001st prime number?
 package code;
 public class q7 {
 
 public static void main(String args[])
-{
-int i,j,c=0,c1=0;
-		for(i=1;;i++)
-		{ c=0;
-			for(j=1;j<=i;j++)
-			{
-			if(i%j==0)
-			{
-				c++;
-			}
-		}
-			if(c==2)
-
-				c1++;
-
-			if(c1==10001)
-				break;
+{int t=3,k=1;
+	while(k<10001) { int c=0;
+		for(int i=3;i<=t/2;i+=2) {
+			if(t%i==0)
+			{	c++;break;}}
+		if(c==0)
+		{
+			k++;
+		}t+=2;
 	}
-		System.out.println("the 10001st prime number ="+i);	
-  }
+	System.out.println((t-2));
 }
+}
+
 //output:
-//the 10001st prime number =104743
+//104743
