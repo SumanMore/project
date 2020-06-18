1366747d4da429d46fc675871c7d781798c44587
SumanMore <67056908+SumanMore@users.noreply.github.com>
2020-06-18 11:54:45 +0530

    Update q7.java

--- q7.java
+++ q7.java
@@ -1,21 +1,30 @@
 //What is the 10 001st prime number?
 package code;
 public class q7 {
+static boolean isprime(int i) 
+{
+for(int j=3;j<=Math.sqrt(i);j+=2)
+{
+if(i%j==0)
+return false;
+}
+return true;
 
-public static void main(String args[])
-{int t=3,k=1;
-	while(k<10001) { int c=0;
-		for(int i=3;i<=t/2;i+=2) {
-			if(t%i==0)
-			{	c++;break;}}
-		if(c==0)
-		{
-			k++;
-		}t+=2;
-	}
-	System.out.println((t-2));
+}
+public static void main(String args[]) {
+int n=2;
+int t=3;
+int p=3;
+while(n<=10001) {
+if(isprime(t))
+n++;
+p=t;
+t+=2;
+}
+System.out.println(p);
 }
 }
+
 
 //output:
 //104743
