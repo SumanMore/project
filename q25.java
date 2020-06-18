66cb676ecbc467830136423cb08b2bc3ea3fa9a6
SumanMore <67056908+SumanMore@users.noreply.github.com>
2020-06-18 18:30:51 +0530

    Update q25.java

--- q25.java
+++ q25.java
@@ -4,33 +4,35 @@ package code;
 public class q25
 	{public static void main(String[] args) {
 		int ar[]= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-		long a = lcm(ar);
-		System.out.println(a);
+	long a = lcm(ar);
+	System.out.println(a);
 }
-	static long lcm(int arr[])
+static long lcm(int arr[])
+{
+	long l=1;
+	int j=2;
+	while (true)
 	{
-		long l=1;
-		int d=2;
-		while (true)
+		int ctr=0;boolean b=false;
+		for (int i=0;i<arr.length;i++)
 		{
-			int c=0;boolean b=false;
-			for (int i=0;i<arr.length;i++)
+		if(arr[i]==1)
+				ctr++;
+			if(arr[i]%j==0)
 			{
-			if(arr[i]==1)
-					c++;
-				if(arr[i]%d==0)
-				{
-					b=true;
-					arr[i]=arr[i]/d;
-				}}
-			if(b)
-				l=l*d;
-			else 
-				d++;
-			if(c==arr.length)
-				return l;
-			
-		}}}
+				b=true;
+				arr[i]=arr[i]/j;
+			}}
+		if(b)
+			l=l*j;
+		else 
+			j++;
+		if(ctr==arr.length)
+			return l;
+		
+		}		
+		}
+	}
 		
 //output
 //232792560
