d06766ce7ef7ca49187a9f5a9138756cc022785d
SumanMore <67056908+SumanMore@users.noreply.github.com>
2020-06-18 10:45:57 +0530

    Update q25.java

--- q25.java
+++ q25.java
@@ -2,19 +2,15 @@
 //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 package code;
 public class q25
-	
-{
-	 public static void  main(String args[])
-	 {
-	 for(int k=20;k>=1;k+=20) {
+	{public static void main(String[] args) {	// TODO Auto-generated method stub
+		for(int k=20;k>=1;k+=20) {
 			if(divisiable(k)) {
 				System.out.println(k);
 				break;
-			}
-		}
-	}
-
-	private static boolean divisiable(int a) {int ctr=0;
+			}}}
+		
+private static boolean divisiable(int a)
+{int ctr=0;
 		for(int j=1;j<=20;j++) {
 			if(a%j==0) 
 				ctr++;
@@ -23,9 +19,6 @@ public class q25
 		else 
 			return false;
 	}
-
-
-	
-}
+}	
 //output
 //232792560
