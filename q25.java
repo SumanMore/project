e2ee17723759c731699f2c74db4950f4b4ce7ac4
SumanMore <67056908+SumanMore@users.noreply.github.com>
2020-06-19 21:21:52 +0530

    Update q25.java

--- q25.java
+++ q25.java
@@ -1,7 +1,7 @@
 
 //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 package code;
-public class q25
+public class q5
 	{public static void main(String[] args) {
 		int ar[]= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 	long a = lcm(ar);
